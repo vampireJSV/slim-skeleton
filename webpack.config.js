056e5e76ffef7be25e7f2b642f9961b9aecc2bef
daniel <kontakt@daniel-kahl.de>
2017-06-04 15:57:18 +0200

    #no-ticket - quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 "use strict";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const
-    path = require('path'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    ManifestPlugin = require('webpack-manifest-plugin'),
-    file_name_pattern = process.env.DEBUG == 1 ? '[name]' : '[name].[chunkhash]',
+    path = require("path"),
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    ManifestPlugin = require("webpack-manifest-plugin"),
+    file_name_pattern = process.env.DEBUG == 1 ? "[name]" : "[name].[chunkhash]",
     extract_sass = new ExtractTextPlugin(`${file_name_pattern}.css`);
 
 
 module.exports = {
     entry: {
-        main: path.join(__dirname, 'resources/assets/js/main.js'),
+        main: path.join(__dirname, "resources/assets/js/main.js"),
     },
     output: {
-        path: path.join(__dirname, 'public/build'),
+        path: path.join(__dirname, "public/build"),
         filename: `${file_name_pattern}.js`
     },
 
@@ -29,7 +29,7 @@ module.exports = {
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|jpg|jpeg|png|gif)$/,
-                loader: 'file-loader?name=./assets/[name].[ext]'
+                loader: "file-loader?name=./assets/[name].[ext]"
             }
         ]
     },
