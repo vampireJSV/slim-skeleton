318dca35c9eb8e8446daaf973d1391530f354950
Josevi Canet <josevisw@gmail.com>
2018-05-14 13:26:46 +0200

    php 7.2 update
    change command line

--- app/Command/DevCommand.php
+++ app/Command/DevCommand.php
@@ -5,5 +5,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 class DevCommand {
 	public function __invoke( OutputInterface $output ) {
 		shell_exec( "php -S " . mount_url( false ) . " -t public index.php" );
+		//php 7.2 shell_exec( "php -S " . mount_url( false ) . " -t public" );
 	}
 }
