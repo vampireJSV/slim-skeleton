ff5d57fb739d6f0ca3c24c6f0abe124958141b6a
Josevi <josevi@creativados.com>
2020-02-27 10:08:18 +0100

    webp disabled

--- webpack.config.js
+++ webpack.config.js
@@ -71,9 +71,6 @@ module.exports = {
                             },
                             gifsicle: {
                                 interlaced: false,
-                            },
-                            webp: {
-                                quality: 75
                             }
                         },
                     },
